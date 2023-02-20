ced270d039141ff7db4f6283625de6a9690e7443
Alex Mark <alexvmar@yandex.ru>
2023-02-20 23:17:37 +0300

    changed2 file3 homework3

--- task_3_3/Program.cs
+++ task_3_3/Program.cs
@@ -9,9 +9,8 @@ Console.Clear();
 
 Console.Write("Введите число отличное от нуля: ");
 int num = int.Parse(Console.ReadLine()!);
-double len = Math.Sqrt(Math.Pow(num, 2));  // тут вывожу длину массива гарантированным положительным числом
-int len_int = (int)len; // преобразую в целое число, иначе выдаёт ошибку на использование как длину массива
-double[] table = new double[len_int];
+int len = (int)Math.Sqrt(Math.Pow(num, 2)); // тут вывожу длину массива гарантированным положительным числом
+double[] table = new double[len];
 int j = 0;
 int num_tmp = 1;
 int num_ngt = num;  // делаю параллельную переменную, чтобы в выводе всё корректно отображалось. 
@@ -26,7 +25,7 @@ if (num_ngt < 0)
 }
 else
 {
-    for (; num_tmp <= len_int; num_tmp++)
+    for (; num_tmp <= len; num_tmp++)
     {
         table[j] = Math.Pow(num_tmp, 3);
         j++;
